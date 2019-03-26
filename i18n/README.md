9484dd7ae6d4e3dc37bc731bcc9c3f644e3e95f8
unknown <kasperkasperfish@gmail.com>
2019-03-26 23:56:49 +0100

    add i18n

---
+++ i18n/README.md
@@ -0,0 +1,2 @@
+# memberclient_i18n
+default laguage strings used in the eosDAC memberclient
---
+++ i18n/index.js
@@ -0,0 +1,25 @@
+
+import enUS from './en_US'
+// import de from './de'
+// import enGB from './en_GB'
+// import es from './es'
+// import fr from './fr'
+// import it from './it'
+// import ja from './ja'
+// import ko from './ko'
+// import ru from './ru'
+// import vi from './vi'
+// import zhHANS from './zh_CN'
+export default {
+  'en-us': enUS
+  // ,'de': de,
+  // 'en-gb': enGB,
+  // 'es': es,
+  // 'fr': fr,
+  // 'it': it,
+  // 'ja': ja,
+  // 'ko': ko,
+  // 'ru': ru,
+  // 'vi': vi,
+  // 'zh-hans': zhHANS
+}
