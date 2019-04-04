9b2520f447d6b107be39d19de1b8d29f80cc517a
unknown <kasperkasperfish@gmail.com>
2019-04-04 13:17:14 +0200

    added credits and external links

--- components/menu/menu-extension.vue
+++ components/menu/menu-extension.vue
@@ -1,18 +1,45 @@
 <template>
   <div>
-    <q-item to="/example">
+    <!-- <q-item to="/example">
       <q-item-side icon="check" color="text2" />
       <q-item-main :label="$t('default.example_page')" class="text-text1" />
+    </q-item> -->
+    <q-item to="/credits">
+      <q-item-side :icon="$configFile.icon.credits" color="text2" />
+      <q-item-main :label="$t('menu.credits')" class="text-text1" />
+    </q-item>
+
+    <q-item
+      @click.native="openURL('https://explorer.eosdac.io')"
+      class="cursor-pointer"
+    >
+      <q-item-side :icon="$configFile.icon.explorer" color="text2" />
+      <q-item-main :label="$t('menu.token_explorer')" class="text-text1" />
+      <q-item-side right icon="icon-transfer-out" color="text2" />
+    </q-item>
+
+    <q-item
+      @click.native="openURL('https://discord.io/eosdac')"
+      class="cursor-pointer"
+    >
+      <q-item-side :icon="$configFile.icon.support" color="text2" />
+      <q-item-main :label="$t('menu.support')" class="text-text1" />
+      <q-item-side right icon="icon-transfer-out" color="text2" />
     </q-item>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import { openURL } from "quasar";
+
 export default {
   name: "menuExtension",
   data() {
     return {};
+  },
+  methods: {
+    openURL
   },
   computed: {
     ...mapGetters({
--- router/routes.js
+++ router/routes.js
@@ -2,7 +2,10 @@ const routes_extension = [
   {
     path: "/",
     component: () => import("layouts/MyLayout.vue"),
-    children: [{ path: "example", component: () => import("../pages/example") }]
+    children: [
+      { path: "example", component: () => import("../pages/example") },
+      { path: "credits", component: () => import("../pages/credits.vue") }
+    ]
   }
 ];
 
