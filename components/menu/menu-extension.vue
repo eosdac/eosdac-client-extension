4336fe0ce8ed6c1b9504811c484403adbfa9c637
unknown <kasperkasperfish@gmail.com>
2019-04-04 15:17:19 +0200

    add all config jsons

--- components/menu/menu-extension.vue
+++ components/menu/menu-extension.vue
@@ -4,18 +4,13 @@
       <q-item-side icon="check" color="text2" />
       <q-item-main :label="$t('default.example_page')" class="text-text1" />
     </q-item> -->
-    <q-item to="/credits">
-      <q-item-side :icon="$configFile.icon.credits" color="text2" />
-      <q-item-main :label="$t('menu.credits')" class="text-text1" />
-    </q-item>
-
     <q-item
       @click.native="openURL('https://explorer.eosdac.io')"
       class="cursor-pointer"
     >
       <q-item-side :icon="$configFile.icon.explorer" color="text2" />
       <q-item-main :label="$t('menu.token_explorer')" class="text-text1" />
-      <q-item-side right icon="icon-transfer-out" color="text2" />
+      <q-item-side right icon="link" color="text2" />
     </q-item>
 
     <q-item
@@ -24,7 +19,12 @@
     >
       <q-item-side :icon="$configFile.icon.support" color="text2" />
       <q-item-main :label="$t('menu.support')" class="text-text1" />
-      <q-item-side right icon="icon-transfer-out" color="text2" />
+      <q-item-side right icon="link" color="text2" />
+    </q-item>
+
+    <q-item to="/credits">
+      <q-item-side :icon="$configFile.icon.credits" color="text2" />
+      <q-item-main :label="$t('menu.credits')" class="text-text1" />
     </q-item>
   </div>
 </template>
