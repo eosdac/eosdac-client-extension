aef3dae5baa083f86317eea952e8370c1aafd0bd
unknown <kasperkasperfish@gmail.com>
2019-04-05 11:37:36 +0200

    add verify bot token page

--- router/routes.js
+++ router/routes.js
@@ -3,8 +3,18 @@ const routes_extension = [
     path: "/",
     component: () => import("layouts/MyLayout.vue"),
     children: [
-      { path: "example", component: () => import("../pages/example") },
-      { path: "credits", component: () => import("../pages/credits.vue") }
+      {
+        path: "example",
+        component: () => import("../pages/example")
+      },
+      {
+        path: "credits",
+        component: () => import("../pages/credits.vue")
+      },
+      {
+        path: "verify/:token",
+        component: () => import("../pages/verify-token")
+      }
     ]
   }
 ];
